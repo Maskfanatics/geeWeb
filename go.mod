821833ee23805609e9877131d51547006b513955
shujin <shujin.jiang@greatopensource.com>
2024-04-19 10:46:47 +0800

    i

---
+++ go.mod
@@ -0,0 +1,7 @@
+module geeWeb
+
+go 1.22.1
+
+require gee v0.0.0
+
+replace gee => ./gee
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"gee"
+	"log"
+	"net/http"
+	"time"
+)
+
+func onlyForV2() gee.HandlerFunc {
+	return func(c *gee.Context) {
+		// Start timer
+		t := time.Now()
+		c.Fail(500, "Internl Server Error")
+		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
+	}
+}
+
+func main() {
+	r := gee.New()
+	r.Use(gee.Logger())
+	r.GET("/index", func(ctx *gee.Context) {
+		ctx.HTML(http.StatusOK, "<h1>Index Page</h1>")
+	})
+
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(ctx *gee.Context) {
+			ctx.HTML(http.StatusOK, "<h1>hello gee</h1>")
+		})
+		v1.GET("/hello", func(ctx *gee.Context) {
+			ctx.String(http.StatusOK, "hello %s ,you're at %s\n", ctx.Query("name"), ctx.Path)
+		})
+	}
+
+	v2 := r.Group("/v2")
+	v2.Use(onlyForV2())
+	{
+		v2.GET("/hello/:name", func(ctx *gee.Context) {
+			ctx.String(http.StatusOK, "hello %s , you're at %s\n", ctx.Param("name"), ctx.Path)
+		})
+		v2.POST("/login", func(ctx *gee.Context) {
+			ctx.JSON(http.StatusOK, gee.H{
+				"username": ctx.PostFrom("username"),
+				"password": ctx.PostFrom("pasword"),
+			})
+		})
+	}
+
+	r.Run(":9999")
+}
